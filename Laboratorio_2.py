78ae48263cbafb2efbe30d3af4288a8f05742365
wizard <canelafix@gmail.com>
2023-11-11 15:23:25 -0500

    cambio

--- Laboratorio_2.py
+++ Laboratorio_2.py
@@ -30,6 +30,65 @@ if len(y.shape)==2:
     channel2 = y[:,1]
     y = (channel1 + channel2)/2
 
+# Solicitamos al usuario que seleccione el tipo de filtro FIR o IIR
+seleccion = input("Qué tipo de filtro desea aplicar, ¿FIR o IIR?: ")
 
+if seleccion == "FIR":
 
+    print("Qué tipo de filtro desea: ")    
+    
+    btype_filtros = ["Pasa-bajas", "Pasa-altas", "Pasa-banda", "Rechaza-banda"]
 
+    print("Filtros disponibles:")
+    for i, x in enumerate(btype_filtros):
+        print(f"{i + 1}) {x}")
+
+    btype_S = int(input("Ingrese el tipo de filtro que desea: "))
+
+    if btype_S == 1:
+        btype = 'lowpass'
+    elif btype_S == 2:
+        btype = 'highpass'
+    elif btype_S == 3:
+        btype = 'bandpass'
+    elif btype_S == 4:
+        btype = 'bandstop'
+
+################################################################
+
+    # Lista de filtros FIR disponibles
+    senales = ["Enventanado", "Muestreo en frecuencia", "Parks-McClellan"]
+
+    #print(señales)
+    # Mostramos el menú
+    print("Tipos de filtros FIR disponibles:")
+    for i, x in enumerate(senales):
+        print(f"{i + 1}) {x}")
+
+    seleccion_1 = int(input("Ingrese su selección: "))
+
+    if seleccion_1 == 1:
+
+    elif seleccion_1 == 2:
+
+    elif seleccion_1 == 3:
+
+        
+elif seleccion == "IIR":
+
+    # Lista de filtros IIR disponibles
+    senales_1 = ["Butterworth", "Chebyshov I", "Chebyshov II", "Elíptico"]
+
+    #print(señales)
+    # Mostramos el menú
+    print("Tipos de filtros IIR disponibles:")
+    for i, x in enumerate(senales_1):
+        print(f"{i + 1}) {x}")
+
+    seleccion_2 = int(input("Ingrese su selección: "))
+
+    if seleccion_2 == 1:
+
+    elif seleccion_2 == 2:
+    
+    elif seleccion_2 == 3:
