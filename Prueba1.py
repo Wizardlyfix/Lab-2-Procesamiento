3a7b41d7d8465a9b1ead8616011442e98feec070
wizard <wizardlyfix@outlook.es>
2023-11-15 14:57:31 -0500

    a

--- Prueba1.py
+++ Prueba1.py
@@ -7,7 +7,7 @@ from IPython.display import Audio
 import sounddevice as sd
 import soundfile as sf
 
-y, Fs = librosa.load('\\Users\wizar\OneDrive - INSTITUTO TECNOLOGICO METROPOLITANO - ITM\Procesamiento de Señales\Laboratorio_2\Grabacion.wav')
+y, Fs = librosa.load('/Users/wizard/Library/CloudStorage/OneDrive-INSTITUTOTECNOLOGICOMETROPOLITANO-ITM/Procesamiento de Señales/Laboratorio_2/Grabacion.wav')
 
 if len(y.shape)==2:
     # if audio is stereo
@@ -20,4 +20,19 @@ b,a = signal.cheby2(5, 30, [1e3, 3e3], btype='bandpass', analog=False, output='b
 #w = np.linspace(0, np.pi, int(10e3))
 w, H = signal.freqz(b,a)
 plt.plot(w*Fs/(2*np.pi),np.abs(H))
+plt.show()
+
+angles = np.unwrap(np.angle(H))
+plt.plot(w*Fs/(2*np.pi), angles*180/np.pi)
+plt.xlabel("Frecuencia [Hz]")
+plt.ylabel('Fase [°]')
+plt.title("Respuesta en frecuencia de Fase")
+plt.show()
+
+#####################################MAPA DE CALOR##########################################
+
+f1, t1, Sxx = signal.spectrogram(y, Fs)
+plt.pcolormesh(t1, f1, 20*np.log10(Sxx), shading='gouraud')
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
 plt.show()
