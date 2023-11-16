48cc523849b05fca9a79e672b6604609b7d9df6e
wizard <wizardlyfix@outlook.es>
2023-11-16 13:14:52 -0500

    revisa el notebook tight_layout

---
+++ demo_gridspec01.py
@@ -0,0 +1,37 @@
+"""
+=================
+subplot2grid demo
+=================
+
+This example demonstrates the use of `.pyplot.subplot2grid` to generate
+subplots.  Using `.GridSpec`, as demonstrated in
+:doc:`/gallery/userdemo/demo_gridspec03` is generally preferred.
+"""
+
+import matplotlib.pyplot as plt
+
+plt.rcParams['savefig.facecolor'] = "0.8"
+
+
+def example_plot(ax, fontsize=12):
+    ax.plot([1, 2])
+
+    ax.locator_params(nbins=3)
+    ax.set_xlabel('x-label', fontsize=fontsize)
+    ax.set_ylabel('y-label', fontsize=fontsize)
+    ax.set_title('Title', fontsize=fontsize)
+
+plt.close('all')
+fig = plt.figure()
+
+ax1 = plt.subplot2grid((3, 3), (0, 0))
+ax2 = plt.subplot2grid((3, 3), (0, 1), colspan=2)
+ax3 = plt.subplot2grid((3, 3), (1, 0), colspan=2, rowspan=2)
+ax4 = plt.subplot2grid((3, 3), (1, 2), rowspan=2)
+
+example_plot(ax1)
+example_plot(ax2)
+example_plot(ax3)
+example_plot(ax4)
+
+plt.tight_layout()
